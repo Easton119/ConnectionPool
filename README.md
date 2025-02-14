5be2cd9496a9ded358121e5012b8eea9e196cfe9
qyj <1137109222@qq.com>
2025-02-14 17:04:35 +0800

    docs:readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ConnectionPool
 ConnectionPool.cpp和ConnectionPool.h：连接池代码实现  
 Connection.cpp和Connection.h：数据库操作代码、增删改查代码实现  
-
+mysql.cnf：数据库连接配置
+main.cpp：分别进行使用连接池和未使用连接池，单线程与多线程的性能测试
 ## 项目结构
 .  
 ├── include  
@@ -16,7 +17,7 @@ Connection.cpp和Connection.h：数据库操作代码、增删改查代码实现
     ├── ConnectionPool.cpp  
     └── main.cpp  
   
-## 环境配置与编译  
-
+## 项目环境与编译  
+Ubuntu 22.04
 编译命令：`g++ src/*.cpp -o main -Iinclude -lmysqlclient`  
 运行：`./main`
--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,33 @@
 using namespace std;
 
 int main(){
-    
-    //单线程：
     clock_t begin = clock();
+    //单线程：
+    // cout<<"单线程"<<endl;
     // ConnectionPool *cp = ConnectionPool::getConnectionPool();
     // for(int i=0;i<10000;i++){
-        /*Connection conn;
-        char sql[1024]={0};
-        string s = "testt"+to_string(i);
-        // cout<<s.c_str()<<endl;
-        sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
-        conn.connect("127.0.0.1",3306,"root","123456","chat");
-        conn.update(sql);*/
+    //     /*Connection conn;
+    //     char sql[1024]={0};
+    //     string s = "testt"+to_string(i);
+    //     // cout<<s.c_str()<<endl;
+    //     sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
+    //     conn.connect("127.0.0.1",3306,"root","123456","chat");
+    //     conn.update(sql);*/
         
     //     shared_ptr<Connection>sp= cp->getConnection();
     //     char sql[1024]={0};
     //     string s = "testt"+to_string(i);
-    //     // cout<<s.c_str()<<endl;
+    //      // cout<<s.c_str()<<endl;
     //     sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
     //     sp->update(sql);
 
     // }
 
-    //多线程
+    // 多线程
+    cout<<"多线程"<<endl;
     thread t1([](){
         ConnectionPool *cp = ConnectionPool::getConnectionPool();
-        for(int i=0;i<5000;i++){
+        for(int i=0;i<2500;i++){
              shared_ptr<Connection>sp= cp->getConnection();
              char sql[1024]={0};
              string s = "testt"+to_string(i);
@@ -40,7 +41,7 @@ int main(){
     });
     thread t2([](){
         ConnectionPool *cp = ConnectionPool::getConnectionPool();
-        for(int i=0;i<5000;i++){
+        for(int i=0;i<2500;i++){
              shared_ptr<Connection>sp= cp->getConnection();
              char sql[1024]={0};
              string s = "testt"+to_string(i);
@@ -49,32 +50,32 @@ int main(){
              sp->update(sql);
         }
     });
-    // thread t3([](){
-    //     ConnectionPool *cp = ConnectionPool::getConnectionPool();
-    //     for(int i=0;i<2500;i++){
-    //          shared_ptr<Connection>sp= cp->getConnection();
-    //          char sql[1024]={0};
-    //          string s = "testt"+to_string(i);
-    //          // cout<<s.c_str()<<endl;
-    //          sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
-    //          sp->update(sql);
-    //     }
-    // });
-    // thread t4([](){
-    //     ConnectionPool *cp = ConnectionPool::getConnectionPool();
-    //     for(int i=0;i<2500;i++){
-    //          shared_ptr<Connection>sp= cp->getConnection();
-    //          char sql[1024]={0};
-    //          string s = "testt"+to_string(i);
-    //          // cout<<s.c_str()<<endl;
-    //          sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
-    //          sp->update(sql);
-    //     }
-    // });
+    thread t3([](){
+        ConnectionPool *cp = ConnectionPool::getConnectionPool();
+        for(int i=0;i<2500;i++){
+             shared_ptr<Connection>sp= cp->getConnection();
+             char sql[1024]={0};
+             string s = "testt"+to_string(i);
+             // cout<<s.c_str()<<endl;
+             sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
+             sp->update(sql);
+        }
+    });
+    thread t4([](){
+        ConnectionPool *cp = ConnectionPool::getConnectionPool();
+        for(int i=0;i<2500;i++){
+             shared_ptr<Connection>sp= cp->getConnection();
+             char sql[1024]={0};
+             string s = "testt"+to_string(i);
+             // cout<<s.c_str()<<endl;
+             sprintf(sql,"insert into user(name,password) values('%s','%s')",s.c_str(),"123");
+             sp->update(sql);
+        }
+    });
     t1.join();
     t2.join();
-    // t3.join();
-    // t4.join();
+    t3.join();
+    t4.join();
     clock_t end =clock();
     double endtime=(double)(end-begin)/CLOCKS_PER_SEC;
     cout<<"Over time:"<< endtime * 1000<< "ms" << endl;
